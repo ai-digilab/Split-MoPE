ab051da0ce8c06ae9d30e0d519b177ec14bd88e7
jirureta <j.iruretabarrena@gmail.com>
2026-04-21 09:55:43 +0000

    delete untracked files

--- models/.ipynb_checkpoints/dino_encoders-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import torch
-import time
-
-def get_dino_encoder(model_ref='dinov2_vits14'):
-    '''
-    Func that downloads a pretrained dinoV2 encoder
-    '''
-    dino_vit = torch.hub.load('facebookresearch/dinov2', model_ref)
-    return dino_vit
